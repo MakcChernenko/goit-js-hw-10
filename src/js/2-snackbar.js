23e4dbd6e88ea06194f3437fa7449e503fd442f5
MakcChernenko <157323123+MakcChernenko@users.noreply.github.com>
2024-06-15 14:59:32 +0300

    added 2 task

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,38 @@
-console.log('hello snackbar');
+import iziToast from 'izitoast';
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const delay = parseInt(this.elements.delay.value);
+  const state = this.elements.state.value;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Успіх',
+        message: `✅ Виконаний проміс за ${delay}мс`,
+        titleColor: 'white',
+        messageColor: 'white',
+        backgroundColor: 'green',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Помилка',
+        message: `❌ Відхилений проміс за ${delay}мс`,
+        titleColor: 'white',
+        messageColor: 'white',
+        backgroundColor: 'red',
+      });
+    });
+
+  this.reset();
+});
